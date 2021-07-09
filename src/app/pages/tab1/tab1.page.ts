e24e06145236ae1861658619587c1df649571010
Marcos Marenna <marcosfm17@gmail.com>
2021-07-09 10:47:22 -0300

    109. pantalla noticia

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NoticiasService } from '../../services/noticias.service';
+import { Article } from '../../interfaces/interfaces';
 
 @Component({
   selector: 'app-tab1',
@@ -7,12 +8,16 @@ import { NoticiasService } from '../../services/noticias.service';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page implements OnInit {
+  noticias: Article[] = [];
 
   constructor(private noticiasService: NoticiasService ) {}
 
   ngOnInit() {
-    this.noticiasService.getTopHeadLines().subscribe(
-      resp => { console.log(resp) }
+    this.noticiasService.getTopHeadLines()
+    .subscribe( resp => {
+        console.log(resp.articles);
+        this.noticias.push(... resp.articles);
+      }
     );
   }
 }
